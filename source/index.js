f8249c8cd380d5be1e5748c1b006c300c4ed43f6
Suherman <suherman@qantas.com.au>
2019-09-14 09:42:07 +1000

    Convert to ES6.

--- source/index.js
+++ source/index.js
@@ -1,4 +1,6 @@
-exports.handler = async function (event, context) {
+export async function handler(event, context) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
+    return {
+        logStreamName: context.logStreamName
+    }
 }
